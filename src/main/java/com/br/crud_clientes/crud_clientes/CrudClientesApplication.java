4858c17d951a8980a89376b5da22a199482ad62f
Laboratório Informática <labinfo@unifil.br>
2019-03-29 18:49:16 -0300

    Inicializa o projeto

---
+++ src/main/java/com/br/crud_clientes/crud_clientes/CrudClientesApplication.java
@@ -0,0 +1,13 @@
+package com.br.crud_clientes.crud_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudClientesApplication.class, args);
+	}
+
+}
